e050a0ad9c56162000b540e3ba478101176ec6fc
jesupemi-ajulo <jesupemii19@gmail.com>
2026-05-04 23:19:16 +0100

    Stylings

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "smart-blue": "#0466c8",
+        sapphire: "#0353a4",
+        "regal-navy": "#023e7d",
+        prussian: "#002855",
+        "prussian-2": "#001845",
+        "prussian-3": "#001233",
+        twilight: "#33415c",
+        "blue-slate": "#5c677d",
+        "slate-grey": "#7d8597",
+        "lav-grey": "#979dac",
+      },
+      fontFamily: {
+        display: ["Syne", "sans-serif"],
+        body: ["DM Sans", "sans-serif"],
+      },
+    },
+  },
+};
